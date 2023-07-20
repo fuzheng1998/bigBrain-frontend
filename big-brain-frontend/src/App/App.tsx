3b4678dbace0b13381afb509fa90fea8297f6802
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-21 09:25:47 +1000

    move app

--- big-brain-frontend/src/App/App.tsx
+++ big-brain-frontend/src/App/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
-import LoginPage from "../LoginPage";
-
+import LoginPage from "../pages/LoginPage";
+/**
+ * @description The main App component, global layout for the entire app.
+ */
 function App() {
   return (
     <div className="App">
