0db6044371d2ee2d12c911c91b5ea1f30f7a107a
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-23 19:03:00 +1000

    move page component without props

--- big-brain-frontend/src/pages/DetailPage/DetailPage.tsx
+++ big-brain-frontend/src/pages/DetailPage/DetailPage.tsx
@@ -1,21 +1,43 @@
-import { Question } from '../../types/question';
+import { QType } from '../../types/question';
 import QuestionList from '../../components/QuestionList';
 
-interface DetailPageProps {
-  id: string;
-  title: string;
-  description: string;
-  questions: Question[];
-}
 /**
  * @description DetailPage for one specific game, including game metadata and question list.
  */
-function DetailPage({
-  id,
-  title,
-  description,
-  questions,
-}: DetailPageProps) {
+function DetailPage() {
+  //   data fetched from backend should be used here
+  const {
+    id,
+    title,
+    description,
+    questions,
+  } = {
+    id: '101',
+    title: 'Product XYZ',
+    description: 'This is a detailed description of Product XYZ.',
+    questions: [
+      {
+        id: '1',
+        type: QType.SINGLE,
+        question: 'What is the capital of France?',
+        timeLimit: 10,
+        points: 100,
+        media: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1200px-Flag_of_France.svg.png',
+        answers: [
+          {
+            id: '1',
+            answer: 'Paris',
+            isCorrect: true,
+          },
+          {
+            id: '2',
+            answer: 'Berlin',
+            isCorrect: false,
+          },
+        ],
+      },
+    ],
+  };
   return (
     <div>
       <h1>DetailPage</h1>
