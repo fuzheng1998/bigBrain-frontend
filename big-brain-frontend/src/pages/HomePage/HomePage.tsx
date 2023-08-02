00b9eff990cf7430e3954ec54cdaae9b9acc2f5f
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-08-02 18:00:17 +1000

    home page layout and heroicon lib

--- big-brain-frontend/src/pages/HomePage/HomePage.tsx
+++ big-brain-frontend/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,21 @@
 /**
  * @file This file is the home page of the application.
  */
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div className="HomePage">
-      <h1>Welcome to BigBrain</h1>
+    <div className="flex h-screen">
+      <div className="bg-amber-200 basis-1/2 flex justify-center items-center">
+        <Link to="/admin"> admin? click here</Link>
+      </div>
+      <div className="bg-amber-700 basis-1/2 flex justify-center items-center">
+        <Link to="/join">
+          player? click here
+        </Link>
+      </div>
     </div>
   );
 }
+
 export default HomePage;
